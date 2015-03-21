5f79b2eb069070bb0b45cf610c4fa76a6a6a4c9a
Alex Risberg <alex.risberg@gmail.com>
2015-03-21 13:07:12 -0700

    added math functiions

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 
 window.onload = function () {
 var input = 0;
+var firstValue = 0;
+var secondValue= 0;
+var operator = null;
+
 function getInput() {
 	input = (this.firstChild.innerHTML);
 	document.getElementById('display').firstChild.innerHTML += input;
 }
 
 function storeValue() {
-	var firstValue = input;
+	firstValue = document.getElementById('display').firstChild.innerHTML;
 	input = 0;
+	oper = (this.firstChild.innerHTML);
 	document.getElementById('display').firstChild.innerHTML = "";
 }
 
+function getAnswer() {
+	secondValue = document.getElementById('display').firstChild.innerHTML;
+	document.getElementById('display').firstChild.innerHTML = eval(firstValue + oper + secondValue);
+}
+
+function clearDisplay() {
+	input = 0;
+	firstValue = 0;
+	secondValue= 0;
+	operator = null;
+	document.getElementById('display').firstChild.innerHTML = ""
+}
 
 var buttons = document.getElementsByClassName('button'); 
 	console.log(buttons);
@@ -20,8 +37,19 @@ var buttons = document.getElementsByClassName('button');
 
 	}
 
- var clear = document.getElementById('clear');
- 	clear.addEventListener("click", storeValue);
+var operator = document.getElementsByClassName('op');
+	for (var i = 0; i < operator.length; i++){
+	operator[i].addEventListener("click", storeValue);
+	}
+
+document.getElementById('equals').addEventListener("click", getAnswer);
+document.getElementById('clear').addEventListener("click", clearDisplay);
+
+
+
 
 }
+
+
+
 
